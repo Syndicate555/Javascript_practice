e3b94f6dfa3e62a1e9a6156c3d704336eb0403cb
Saffat Aziz <saziz028@uottawa.ca>
2021-09-16 02:24:21 -0400

    changes

--- index.js
+++ index.js
@@ -9,3 +9,16 @@ const companies = [
   { name: "Company Eight", category: "Technology", start: 2011, end: 2016 },
   { name: "Company Nine", category: "Retail", start: 1981, end: 1989 },
 ];
+
+const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+
+//forEach
+
+// for (let i = 0; i < companies.length; i++) {
+//   console.log(companies[i]);
+// }
+
+// Interates through the whole array
+companies.forEach((company) => {
+  console.log(company.name);
+});
