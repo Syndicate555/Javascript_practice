6630a05fe95ff2448bcd5c2feeb08f70218080a2
Saffat Aziz <saziz028@uottawa.ca>
2021-09-16 02:32:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -23,8 +23,13 @@ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 //   console.log(company.name);
 // });
 
+// filter Finance companies
 companies.filter((company) => {
   if (company.category == "Finance") {
     console.log(company);
   }
 });
+
+// get 21 and older
+const canDrink = ages.filter((age) => age >= 21);
+console.log(canDrink);
