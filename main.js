52e3d395de3931ae0391a2af4703e6cc13931fb8
Saffat Aziz <saziz028@uottawa.ca>
2021-09-27 16:03:13 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,40 +1,43 @@
-let arr1 = [
-  {
-    id: 5547107123394,
-    email: "sseevara235@gmail.com",
-  },
-  {
-    id: 5547107113394,
-    email: "sseevara234@gmail.com",
-  },
-  {
-    id: 554722107123394,
-    email: "sseevara233@gmail.com",
-  },
-  {
-    id: 55471071265394,
-    email: "sseevara232@gmail.com",
-  },
-  {
-    id: 55471074523394,
-    email: "sseevara231@gmail.com",
-  },
-];
+// let arr1 = [
+//   {
+//     id: 5547107123394,
+//     email: "sseevara235@gmail.com",
+//   },
+//   {
+//     id: 5547107113394,
+//     email: "sseevara234@gmail.com",
+//   },
+//   {
+//     id: 554722107123394,
+//     email: "sseevara233@gmail.com",
+//   },
+//   {
+//     id: 55471071265394,
+//     email: "sseevara232@gmail.com",
+//   },
+//   {
+//     id: 55471074523394,
+//     email: "sseevara231@gmail.com",
+//   },
+// ];
 
-let arr2 = [
-  {
-    id: 5547107123394,
-    email: "sseevara235@gmail.com",
-  },
-  {
-    id: 5547107113394,
-    email: "sseevara234@gmail.com",
-  },
-];
+// let arr2 = [
+//   {
+//     id: 5547107123394,
+//     email: "sseevara235@gmail.com",
+//   },
+//   {
+//     id: 5547107113394,
+//     email: "sseevara234@gmail.com",
+//   },
+// ];
 
-let arr3 = [].concat(
-  arr1.filter((obj1) => arr2.every((obj2) => obj1.email !== obj2.email)),
-  arr2.filter((obj2) => arr1.every((obj1) => obj2.email !== obj1.email))
-);
+// let arr3 = [].concat(
+//   arr1.filter((obj1) => arr2.every((obj2) => obj1.email !== obj2.email)),
+//   arr2.filter((obj2) => arr1.every((obj1) => obj2.email !== obj1.email))
+// );
 
-console.log(arr3);
+// console.log(arr3);
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(arr.slice(0, arr.length / 2));
